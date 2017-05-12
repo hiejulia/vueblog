bfc1952faf516d480898f794ff89a2dabded4779
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-12 19:01:13 +0300

    update shopping app with components

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,19 +1,50 @@
  <template>
-   <div id="app">
+   <div id="app" class="container">
      <img src="./assets/logo.png">
-     <h1>{{title}}</h1>
+     <h1>Shopping List</h1>
+     <p>{{msg}}</p>
+     <add-item-component></add-item-component>
+     <items-component :items="items">
+     </items-component>
+    
+
+ <change-title-component></change-title-component>
+
+     <transition name="fade">
+     
+      
+     </transitin>
      
    </div>
 </template>
 
 <script>
+import AddItemComponent from './components/AddItemComponent'
+import ItemsComponent from './components/ItemsComponent'
+import ChangeTitleComponent from './components/ChangeTitleComponent'
+
 export default {
   /* eslint-disable no-new */
-  name: 'app'
+
+components: {
+    AddItemComponent,
+    ItemsComponent,
+    ChangeTitleComponent
+  },
+
+  data () {
+    return {
+        msg: 'Hello!',
+        items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }]
+
+      }
+  }
 }
 </script>
 
-<style>
+
+<!--start with style-->
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,4 +53,11 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+  }
+  .fade-enter, .fade-leave-active {
+    opacity: 0
+  }
 </style>
+
--- blog/src/main.js
+++ blog/src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+
 //import plugin 
 
 /* eslint-disable no-new */
@@ -17,49 +17,8 @@ import router from './router'
 //   })
 // })
 //use plugin > Vue.use(tenplugin)
-var data = {
-  items: [{ text: 'Bananas', checked: true }, { text: 'Apples', checked: false }],
-  title: 'Shopping App',
-  newItem: ''
-};
 
 new Vue({
-  el: '#app',
-  data: data,
-  methods: {
-    addItem: function () {
-      var text;
-
-      text = this.newItem.trim();
-      if (text) {
-        this.items.push({
-          text: text,
-          checked: false
-        });
-        this.newItem = '';
-      }
-    }
-  }
-});
-
-var ItemsComponent = Vue.extend({
-  data: function() {
-    return data;
-
-  },
-  template : {
-    template: '<ul>' +
-  '           <li v-for="item in items" :class="{ \'removed\': item.checked }">' +
-  '             <div class="checkbox">' +
-  '               <label>' +
-  '                 <input type="checkbox" v-model="item.checked"> {{ item.text }}' +
-  '               </label>' +
-  '             </div>' +
-  '           </li>' +
-  '         </ul>'
-  }
-});
-
-Vue.component('items-component',ItemsComponent);
-Vue.component('change-title-component', ChangeTitleComponent);
-Vue.component('add-item-component', AddItemComponent); 
+  el: '#container',
+  components: {App}
+})
