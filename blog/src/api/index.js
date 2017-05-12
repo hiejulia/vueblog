9b5a4ccf2108fdc1838687a30727dd2708ab93de
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-13 00:59:17 +0300

    update server

--- blog/src/api/index.js
+++ blog/src/api/index.js
@@ -3,18 +3,24 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
+
+
 const ShoppingListsResource = Vue.resource('http://localhost:3000/' + 'shoppinglists{/id}')
 
 export default {
+    //fetchshoppinglists
     fetchShoppingLists: () => {
         return ShoppingListsResource.get()
     },
+    //add new shopping list
     addNewShoppingList: (data) => {
     return ShoppingListsResource.save(data)
   },
+  //update shopping list
   updateShoppingList: (data) => {
     return ShoppingListsResource.update({ id: data.id }, data)
   },
+  //delete shopping list
   deleteShoppingList: (id) => {
     return ShoppingListsResource.remove({ id: id })
   }
