d85d4816a5652127b188f1faa8b84086641f0dec
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-16 00:01:12 +0300

    config router main

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -2,27 +2,79 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login';
+import Register from '@/components/Login';
 
 Vue.use(Router)
 
-export default new Router({
+export var router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
-    },
-    {
+    }, {
+      path: '/users',
+      component: Users,
+      meta: {
+        requiresAuth: true,
+        requiresAdmin: true
+      }
+    }, {
+      path: '/register',
+      component: Register,
+      meta: {
+        checksAuth: true
+      }
+    }, {
       path: '/login',
-      name: 'Login',
-      component: Login
-    },
-  
-    // { path: '/editor/:id', name: 'edit-article', component: Editor },
-    // { path: '/article/:id', name: 'article', component: Article },
-    // { path: '/profile/:username', name: 'user', component: Profile },
-    // { path: '/article/:username/favorites', name: 'favorites', component: Favorites },
-    // { path: '*', redirect: { name: 'home' } },
+      component: Login,
+      meta: {
+        checksAuth: true
+      }
+    }
 
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+		if (!auth.user.authenticated) {
+			next({
+				path: '/login',
+				query: { redirect: to.fullPath }
+			})
+		} else {
+			next()
+		}
+	}
+	if (to.matched.some(record => record.meta.requiresAdmin)) {
+		// this route requires auth, check if logged in
+		// if not, redirect to login page.
+		if (auth.user.role == 'admin') {
+			next()
+		} else {
+			next({
+				path: '/',
+				query: { redirect: to.fullPath }
+			})
+		}
+	}
+	if (to.matched.some(record => record.meta.checksAuth)) {
+		// this route requires auth, check if logged in
+		// if not, redirect to login page.
+		if (auth.user.authenticated) {
+			next({
+				path: '/',
+				query: { redirect: to.fullPath }
+			})
+		} else {
+			next()
+		}
+	} else {
+		next() // make sure to always call next()!
+	}
+
 })
+
+
