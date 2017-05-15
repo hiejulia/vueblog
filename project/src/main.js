fdc5c53611c42291c9b35dde11cc6ed35c16e2fe
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-16 00:23:15 +0300

    ....

--- project/src/main.js
+++ project/src/main.js
@@ -3,31 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Router from 'vue-router';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import moment from 'moment';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import VueAuth from '@websanova/vue-auth'
 import VeeValidate from 'vee-validate'
-
+import Resource from 'vue-resource';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 //auth
 import auth from './auth/auth'
-
+auth.checkAuth();
 //store
 // import store from './store';
 
 Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
-// Vue.use(VueAuth, {
-//     auth: AuthBearer,
-//     http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
-//     router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js')
-    
-// });
+
+Vue.use(VeeValidate);
+Vue.use(Resource);
+Vue.use(Router);
+
+
+
+
 //filter for date
 Vue.filter('date', date => moment(date).format('MMMM DD, YYYY'));
 
