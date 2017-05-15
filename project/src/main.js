9e24b9490bca1e452bdfc55eae67de93b1c2d655
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-15 22:11:05 +0300

    import to main

--- project/src/main.js
+++ project/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import moment from 'moment';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 
 Vue.config.productionTip = false
 
