b529af25ec3d1dbda993385c626cea175100d5e2
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-15 22:11:57 +0300

    vue use plugin

--- project/src/main.js
+++ project/src/main.js
@@ -8,6 +8,17 @@ import VueAxios from 'vue-axios';
 import moment from 'moment';
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+//store
+import store from './store';
+
+Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
+//filter for date
+Vue.filter('date', date => moment(date).format('MMMM DD, YYYY'));
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
